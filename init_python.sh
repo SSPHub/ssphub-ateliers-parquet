f4ccd1c9e55fcb382f47c8ebd81dd83c5a7fed41
lgaliana <lino.galiana@insee.fr>
2025-04-16 16:08:19 +0000

    delete

--- init_python.sh
+++ init_python.sh
@@ -5,3 +5,4 @@ cp ssphub-ateliers-parquet/python/_s3.qmd s3.qmd
 cp ssphub-ateliers-parquet/pyproject.toml pyproject.toml
 uv pip install -r pyproject.toml --system
 quarto render s3.qmd --to ipynb --execute
+rm -rf ssphub-ateliers-parquet
