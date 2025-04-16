84f588b8041fd95a51a11329bb000476acdda4a8
lgaliana <lino.galiana@insee.fr>
2025-04-16 16:15:13 +0000

    init python

--- init_python.sh
+++ init_python.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 git clone https://github.com/SSPHub/ssphub-ateliers-parquet --depth 1
+
 cp ssphub-ateliers-parquet/python/_s3.qmd s3.qmd
 cp ssphub-ateliers-parquet/pyproject.toml pyproject.toml
+
 uv pip install -r pyproject.toml --system
 quarto render s3.qmd --to ipynb --execute
+
 rm -rf ssphub-ateliers-parquet
